387aa6742721be7e7bf118603d3b62b89a45e850
KaKi87 <tiana.lemesle@live.fr>
2019-03-26 13:45:21 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,3 +1,80 @@
 # ig-scraper
 
-Instagram web scraper (without auth API)
+Scrape data from Instagram without applying for the authenticated API.
+
+## Features
+
+### Get profile info
+
+```js
+Insta.getProfile('tiana_kaki', profile => {
+	console.log(profile);
+});
+```
+
+```json
+{
+		"id": "6965336095",
+		"name": "Tiana",
+		"pic": "https://scontent-mrs1-1.cdninstagram.com/....jpg",
+		"bio": "",
+		"private": false,
+		"verified": false,
+		"website": "https://tianalemesle.fr/",
+		"followers": 46,
+		"following": 68,
+		"posts": 35,
+		"lastPosts": [
+			"Buth7q-nTnW",
+			"BtwBWbmnXtx",
+			"BtHJSmMhiDY"
+		]
+    }
+```
+
+### Get post data
+
+```js
+Insta.getPost('Buth7q-nTnW', post => {
+	console.log(post);
+})
+```
+
+```json
+{
+	"id": "1812187949221841859",
+	"timestamp": 1530249659,
+	"likes": 8,
+	"comments": 0,
+	"caption": "Vendredi dernier sur l'Ile du Souvenir du Parc de la Tête d'Or, à Lyon.",
+	"location": {
+		"name": "Parc de la Tête d'Or",
+		"city": "Lyon, France"
+	},
+	"author": {
+		"id": "6965336095",
+		"verified": false,
+		"pic": "https://scontent-mrs1-1.cdninstagram.com/....jpg",
+		"username": "tiana_kaki",
+		"name": "Tiana"
+	},
+	"contents": [
+		{
+			"url": "https://scontent-mrs1-1.cdninstagram.com/....jpg",
+			"type": "photo"
+		},
+		{
+			"url": "https://scontent-mrs1-1.cdninstagram.com/....jpg",
+			"type": "photo"
+		}
+	]
+}
+```
+
+`comments = -1` when disabled.
+
+## Planned features
+
+- Support for authentication (private profiles, stories)
+- Post comments list
+- Events : new/deleted post, comment, like
---
+++ index.js
@@ -0,0 +1,79 @@
+'use strict';
+
+const
+	request = require('requestretry'),
+	JSDOM = require('jsdom').JSDOM,
+	parse = document => new JSDOM(document, { runScripts: "dangerously" }).window.document,
+	insta = 'https://instagram.com';
+
+module.exports =  {
+	getProfile(username, callback){
+		request(`${insta}/${username}`,(err, res, body) => {
+			if(res.statusCode === 404)
+				throw new Error('Instagram user not found');
+			body += `<script>document.querySelector('html').innerHTML = JSON.stringify(_sharedData.entry_data.ProfilePage[0].graphql.user)</script>`;
+			try {
+				const user = JSON.parse(parse(body).body.textContent);
+				callback({
+					id: user['id'],
+					name: user['full_name'],
+					pic: user['profile_pic_url_hd'],
+					bio: user['biography'],
+					private: user['is_private'],
+					verified: user['is_verified'],
+					website: user['external_url'],
+					followers: user['edge_followed_by']['count'],
+					following: user['edge_follow']['count'],
+					posts: user['edge_owner_to_timeline_media']['count'],
+					lastPosts: user['edge_owner_to_timeline_media']['edges']
+						.map(post => post['node']['shortcode'])
+				});
+			} catch(e){ throw new Error('Instagram parsing error'); }
+		});
+	},
+	getPost(shortcode, callback){
+		request(`${insta}/p/${shortcode}`, (err, res, body) => {
+			if(res.statusCode === 404)
+				throw new Error('Instagram post not found');
+			body += `<script>document.querySelector('html').innerHTML = JSON.stringify(_sharedData.entry_data.PostPage[0].graphql.shortcode_media)</script>`;
+			try {
+				const post = JSON.parse(parse(body).body.textContent);
+				const p = {
+					id: post['id'],
+					timestamp: post['taken_at_timestamp'],
+					likes: post['edge_media_preview_like']['count'],
+					comments: post['comments_disabled'] ? -1 : post['edge_media_to_comment']['count'],
+					caption: post['edge_media_to_caption']['edges'].length > 0 ? post['edge_media_to_caption']['edges'][0]['node']['text'] : null,
+					location: post['location'] ? {
+						name: post['location']['name'],
+						city: JSON.parse(post['location']['address_json'])['city_name']
+					} : null,
+					author: {
+						id: post['owner']['id'],
+						verified: post['owner']['is_verified'],
+						pic: post['owner']['profile_pic_url'],
+						username: post['owner']['username'],
+						name: post['owner']['full_name']
+					}
+				};
+				switch(post['__typename']){
+					case 'GraphImage':
+						callback(Object.assign(p, {
+							type: post['is_video'] ? 'video': 'photo',
+							contents: [ post['display_url'] ],
+						}));
+						break;
+					case 'GraphSidecar':
+						callback(Object.assign(p, {
+							contents: post['edge_sidecar_to_children']['edges']
+								.map(content => ({
+									url: content['node']['display_url'],
+									type: content['node']['is_video'] ? 'video' : 'photo'
+								}))
+						}));
+						break;
+				}
+			} catch(e){ throw new Error('Instagram parsing error'); }
+		});
+	}
+};
