21fb24afd71a728f37a718e996b27aa1d9cd627a
KaKi87 <tiana.lemesle@live.fr>
2019-04-22 23:26:15 +0200

    Scope adjustment

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 'use strict';
 
+/*
+Modules
+ */
+
 const
 	request = require('requestretry'),
 	JSDOM = require('jsdom').JSDOM,
@@ -7,60 +11,73 @@ const
 	parse = document => new JSDOM(document, { runScripts: 'dangerously' }).window.document,
 	insta = 'https://www.instagram.com/';
 
+/*
+Utils
+ */
+
 const querystring = object => Object.keys(object).map(key => `${key}=${object[key]}`).join('&');
 
-const postDetails = post => ({
-	shortcode: post['node']['shortcode'],
-	caption: post['node']['edge_media_to_caption']['edges'].length > 0
-		? post['node']['edge_media_to_caption']['edges'][0]['node']['text'] : null,
-	comments: post['node']['edge_media_to_comment']['count'],
-	likes: post['node']['edge_liked_by']['count'],
-	thumbnail: post['node']['display_url']
-});
+/*
+Class private variables & methods
+ */
+
+const _this = {
+	sessionID: null,
+	profile: null,
+	get: (path, tryParse = true, sessionID = _this.sessionID, params) => new Promise((resolve, reject) => {
+		const url = insta + path + '/?' + querystring({ ...(sessionID ? { __a: 1 } : {}), ...params });
+		request(url, {
+			headers: {
+				cookie: sessionID ? `sessionid=${sessionID}` : ''
+			},
+			followAllRedirects: true
+		}, (err, res, body) => {
+			if(res.statusCode !== 200){
+				reject(res.statusCode);
+			}
+			else if(tryParse){
+				let data;
+				if(!sessionID){
+					body += `<script>document.querySelector('html').innerHTML = JSON.stringify(Object.values(window['_sharedData']['entry_data'])[0][0])</script>`;
+					body = parse(body).body.textContent;
+				}
+				try { data = JSON.parse(body); } catch(_){}
+				if(!data)
+					reject(406);
+				else {
+					if(!data['graphql'])
+						reject(204);
+					else
+						resolve(Object.values(data['graphql'])[0]);
+				}
+			}
+			else {
+				res.redirected = res.request.uri.href !== url;
+				resolve(res);
+			}
+		});
+	}),
+	search: query => new Promise((resolve, reject) => _this.get('web/search/topsearch',false, undefined, { context: 'blended', query })
+		.then(res => resolve(JSON.parse(res.body)))
+		.catch(reject)),
+	postDetails: post => ({
+		shortcode: post['node']['shortcode'],
+		caption: post['node']['edge_media_to_caption']['edges'].length > 0
+			? post['node']['edge_media_to_caption']['edges'][0]['node']['text'] : null,
+		comments: post['node']['edge_media_to_comment']['count'],
+		likes: post['node']['edge_liked_by']['count'],
+		thumbnail: post['node']['display_url']
+	})
+};
 
 module.exports = class Insta {
-	_get(path, tryParse = true, sessionID = this.sessionID, params){
-		return new Promise((resolve, reject) => {
-			const url = insta + path + '/?' + querystring({ ...(sessionID ? { __a: 1 } : {}), ...params });
-			request(url, {
-				headers: {
-					cookie: sessionID ? `sessionid=${sessionID}` : ''
-				},
-				followAllRedirects: true
-			}, (err, res, body) => {
-				if(res.statusCode !== 200){
-					reject(res.statusCode);
-				}
-				else if(tryParse){
-					let data;
-					if(!sessionID){
-						body += `<script>document.querySelector('html').innerHTML = JSON.stringify(Object.values(window['_sharedData']['entry_data'])[0][0])</script>`;
-						body = parse(body).body.textContent;
-					}
-					try { data = JSON.parse(body); } catch(_){}
-					if(!data)
-						reject(406);
-					else {
-						if(!data['graphql'])
-							reject(204);
-						else
-							resolve(Object.values(data['graphql'])[0]);
-					}
-				}
-				else {
-					res.redirected = res.request.uri.href !== url;
-					resolve(res);
-				}
-			});
-		});
-	}
 	authBySessionID(sessionID){
-		return new Promise((resolve, reject) => this._get('accounts/edit', false, sessionID)
+		return new Promise((resolve, reject) => _this.get('accounts/edit', false, sessionID)
 			.then(res => {
 				if(!res.redirected){
-					this.sessionID = sessionID;
-					this.profile = JSON.parse(res.body)['form_data'];
-					resolve(this.profile);
+					_this.sessionID = sessionID;
+					_this.profile = JSON.parse(res.body)['form_data'];
+					resolve(_this.profile);
 				}
 				else
 					reject(401);
@@ -69,8 +86,8 @@ module.exports = class Insta {
 	}
 	getAccountNotifications(){
 		return new Promise((resolve, reject) => {
-			if(!this.sessionID) return reject(401);
-			this._get('accounts/activity').then(res => {
+			if(!_this.sessionID) return reject(401);
+			_this.get('accounts/activity').then(res => {
 				resolve(res['activity_feed']['edge_web_activity_feed']['edges'].map(item => item['node']).map(notification => ({
 					id: notification['id'],
 					timestamp: notification['timestamp'],
@@ -100,8 +117,8 @@ module.exports = class Insta {
 			})
 		});
 	}
-	getProfile(username = this.profile.username, anonymous = false){
-		return new Promise((resolve, reject) => this._get(username, true, anonymous ? null : undefined)
+	getProfile(username = _this.profile.username, anonymous = false){
+		return new Promise((resolve, reject) => _this.get(username, true, anonymous ? null : undefined)
 			.then(profile => {
 				const access = profile['is_private'] ? !!profile['followed_by_viewer'] : true;
 				resolve({
@@ -115,12 +132,12 @@ module.exports = class Insta {
 					followers: profile['edge_followed_by']['count'],
 					following: profile['edge_follow']['count'],
 					posts: profile['edge_owner_to_timeline_media']['count'],
-					lastPosts: access ? profile['edge_owner_to_timeline_media']['edges'].map(post => postDetails(post)) : null,
+					lastPosts: access ? profile['edge_owner_to_timeline_media']['edges'].map(post => _this.postDetails(post)) : null,
 					link: insta + profile['username'],
 					...(profile['is_business_account'] ? {
 						business: profile['business_category_name']
 					} : {}),
-					...(this.sessionID ? {
+					...(_this.sessionID ? {
 						user: {
 							mutualFollowers: profile['edge_mutual_followed_by']['edges'].map(item => item['node']['username']),
 							blocking: profile['blocked_by_viewer'],
@@ -148,14 +165,14 @@ module.exports = class Insta {
 	getHashtag(hashtag){
 		return new Promise((resolve, reject) => {
 			const path = `explore/tags/${hashtag}`;
-			this._get(path)
+			_this.get(path)
 				.then(hashtag => resolve({
 					pic: hashtag['profile_pic_url'],
 					posts: hashtag['edge_hashtag_to_media']['count'],
-					featuredPosts: hashtag['edge_hashtag_to_top_posts']['edges'].map(post => postDetails(post)),
-					lastPosts: hashtag['edge_hashtag_to_media']['edges'].map(post => postDetails(post)),
+					featuredPosts: hashtag['edge_hashtag_to_top_posts']['edges'].map(post => _this.postDetails(post)),
+					lastPosts: hashtag['edge_hashtag_to_media']['edges'].map(post => _this.postDetails(post)),
 					link: insta + path,
-					...(this.sessionID ? {
+					...(_this.sessionID ? {
 						user: {
 							following: hashtag['is_following']
 						}
@@ -167,7 +184,7 @@ module.exports = class Insta {
 	getLocation(id){
 		return new Promise((resolve, reject) => {
 			const path = `explore/locations/${id}`;
-			this._get(path)
+			_this.get(path)
 				.then(location => {
 					const address = JSON.parse(location['address_json']);
 					resolve({
@@ -182,8 +199,8 @@ module.exports = class Insta {
 						},
 						website: location['website'],
 						phone: location['phone'],
-						featuredPosts: location['edge_location_to_top_posts']['edges'].map(post => postDetails(post)),
-						lastPosts: location['edge_location_to_media']['edges'].map(post => postDetails(post)),
+						featuredPosts: location['edge_location_to_top_posts']['edges'].map(post => _this.postDetails(post)),
+						lastPosts: location['edge_location_to_media']['edges'].map(post => _this.postDetails(post)),
 						link: insta + path
 					});
 				})
@@ -193,7 +210,7 @@ module.exports = class Insta {
 	getPost(shortcode){
 		return new Promise((resolve, reject) => {
 			const path = `p/${shortcode}`;
-			this._get(path)
+			_this.get(path)
 				.then(post => {
 					const
 						caption = post['edge_media_to_caption']['edges'].length > 0
@@ -246,7 +263,7 @@ module.exports = class Insta {
 						caption,
 						hashtags: caption ? caption.match(hashtagsRegex) : null,
 						mentions: caption ? caption.match(usernamesRegex) : null,
-						comments: post['comments_disabled'] ? null : post[`edge_media_${this.sessionID ? 'preview': 'to'}_comment`]['edges']
+						comments: post['comments_disabled'] ? null : post[`edge_media_${_this.sessionID ? 'preview': 'to'}_comment`]['edges']
 							.map(c => ({
 								user: c['node']['owner']['username'],
 								content: c['node']['text'],
@@ -262,13 +279,9 @@ module.exports = class Insta {
 				.catch(reject);
 		});
 	}
-	_search(query){
-		return new Promise((resolve, reject) => this._get('web/search/topsearch',false, undefined, { context: 'blended', query })
-			.then(res => resolve(JSON.parse(res.body)))
-			.catch(reject));
 	}
 	searchProfile(query){
-		return new Promise((resolve, reject) => this._search(query)
+		return new Promise((resolve, reject) => _this.search(query)
 			.then(res => resolve(res['users'].map(item => item['user']).map(profile => ({
 				username: profile['username'],
 				name: profile['full_name'],
@@ -276,7 +289,7 @@ module.exports = class Insta {
 				private: profile['is_private'],
 				verified: profile['is_verified'],
 				followers: profile['follower_count'],
-				...(this.sessionID ? {
+				...(_this.sessionID ? {
 					user: {
 						following: profile['following']
 					}
@@ -285,13 +298,13 @@ module.exports = class Insta {
 			.catch(reject));
 	}
 	searchHashtag(query){
-		return new Promise((resolve, reject) => this._search(query)
+		return new Promise((resolve, reject) => _this.search(query)
 			.then(res => resolve(res['hashtags'].map(item => item['hashtag'])
 				.map(hashtag => ({ name: hashtag['name'], posts: hashtag['media_count'] }))))
 			.catch(reject));
 	}
 	searchLocation(query){
-		return new Promise((resolve, reject) => this._search(query)
+		return new Promise((resolve, reject) => _this.search(query)
 			.then(res => resolve(res['places'].map(item => item['place']['location']).map(location => ({
 				id: location['pk'],
 				name: location['name'],
